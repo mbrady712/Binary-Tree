ce43597aa32a3f91a763018db51d4b3857b58c95
Michael Brady <michaelbrady712@gmail.com>
2023-04-12 13:53:54 -0500

    Added preliminary header file for binary tree

--- bintree.h
+++ bintree.h
@@ -7,8 +7,41 @@ Purpose: This program creates a Binary Search Tree ADT
 #ifndef BINTREE_BINTREE_H
 #define BINTREE_BINTREE_H
 
+#include "data.h";
 
-
+class BinTree{
+    public:
+        BinTree();
+        ~BinTree();
+        bool isEmpty();
+        int getCount();
+        bool getRootData(Data*);
+        void displayTree();
+        //Public methods with private overloads
+        void clear();
+        bool addNode(int, const string*);
+        bool removeNode(int);
+        bool getNode(Data*, int);
+        bool contains(int);
+        int getHeight();
+        void displayPreOrder();
+        void displayPostOrder();
+        void displayInOrder();
+    private:
+        //Attributes
+        DataNode* root;
+        int count;
+        //Overloads of public methods
+        void clear(DataNode*);
+        bool addNode(DataNode*, DataNode**);
+        DataNode* removeNode(int, DataNode*);
+        bool getNode(Data*, int, DataNode*);
+        bool contains(int, DataNode*);
+        int getHeight(DataNode*);
+        void displayPreOrder(DataNode*);
+        void displayPostOrder(DataNode*);
+        void displayInOrder(DataNode*);
+};
 
 #endif /* BINTREE_BINTREE_H */
 
